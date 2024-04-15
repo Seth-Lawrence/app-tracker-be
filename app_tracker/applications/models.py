21b796a77afa9a5f7cb6a580535e9302682d980c
Seth Hobson <sethlawrencehobson@gmail.com>
2024-04-15 08:47:06 -0400

    finished application model

--- app_tracker/applications/models.py
+++ app_tracker/applications/models.py
@@ -1,6 +1,5 @@
 from app_tracker.database import db
 
-
 class Application(db.Model):
 
     __tablename__ = 'applications'
@@ -17,12 +16,77 @@ class Application(db.Model):
         nullable=False
     )
 
+    status = db.Column(
+        db.String(20)
+    )
+
     company_name = db.Column(
         db.String(50),
         nullable=False,
     )
 
     job_title = db.Column(
-        db.String(50)
+        db.String(50),
+        nullable=False
+    )
 
+    remote = db.Column(
+        db.Boolean,
+        nullable=False,
+        default=False
+    )
+
+    city = db.Column(
+        db.String(50),
+        nullable=True
+    )
+
+    state = db.Column(
+        db.String(2),
+        nullable=True
+    )
+
+    min_pay = db.Column(
+        db.Integer
+    )
+
+    max_pay = db.Column(
+        db.Integer
+    )
+
+    cover_letter = db.Column(
+        db.Boolean,
+        nullable=False
+    )
+
+    app_received_confirm = db.Column(
+        db.Boolean
+    )
+
+    num_rounds_reached = db.Column(
+        db.Integer
+    )
+
+    farthest_round_cat = db.Column(
+        db.String(40)
+    )
+
+    outreach = db.Column(
+        db.Boolean
+    )
+
+    outreach_date = db.Column(
+        db.DateTime,
+    )
+
+    outreach_response = db.Column(
+        db.Boolean
+    )
+
+    referral = db.Column(
+        db.Boolean
+    )
+
+    result_date = db.Column(
+        db.DateTime
     )
