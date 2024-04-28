765bee1e3a9b4b05603af04c0cccce0565058bf8
Seth Hobson <sethlawrencehobson@gmail.com>
2024-04-28 19:52:18 -0400

    added static types to do_login method to make it clear that it's not userdata that it takes as parameter but just a username as string

--- app_tracker/auth/models.py
+++ app_tracker/auth/models.py
@@ -6,13 +6,16 @@ class Auth:
     '''model for keeping users logged in / logging out through sessions'''
 
     @classmethod
-    def do_login(self, user):
-        '''logs user in, adds to session'''
+    def do_login(self, username:str) -> None:
+        '''
+        takes username and adds to session
+        to keep user in session
+        '''
 
-        session[USER] = user.id
+        session[USER] = username
 
     @classmethod
-    def logout_user(self):
+    def logout_user(self) -> None:
         '''logs user out --> deletes user from sesison'''
 
         if USER in session:
