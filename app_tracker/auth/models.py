e32939326e5e4a7a6c75217d1a28d832037ac862
Seth Hobson <sethlawrencehobson@gmail.com>
2024-04-28 19:31:36 -0400

    debugging tests after adding auth class

--- app_tracker/auth/models.py
+++ app_tracker/auth/models.py
@@ -6,13 +6,13 @@ class Auth:
     '''model for keeping users logged in / logging out through sessions'''
 
     @classmethod
-    def do_login(user):
+    def do_login(self, user):
         '''logs user in, adds to session'''
 
         session[USER] = user.id
 
     @classmethod
-    def logout_user():
+    def logout_user(self):
         '''logs user out --> deletes user from sesison'''
 
         if USER in session:
