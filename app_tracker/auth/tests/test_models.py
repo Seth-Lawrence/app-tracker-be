ea44dc03905db9725c789748ad6227cf33fbd366
Seth Hobson <sethlawrencehobson@gmail.com>
2024-04-28 20:01:05 -0400

    added todo for tests

--- app_tracker/auth/tests/test_models.py
+++ app_tracker/auth/tests/test_models.py
@@ -2,6 +2,8 @@ from unittest import TestCase
 # from app_tracker.auth.models import Auth
 # from flask import session
 
+#TODO: write tests for authentication classes
+
 class TestAuthentications(TestCase):
 
     def test_login(self):
