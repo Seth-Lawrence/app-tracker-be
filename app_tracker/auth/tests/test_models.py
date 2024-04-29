8376c9b5b8eaf6909ad44a8bd0fd9c506bfe2e6f
Seth Hobson <sethlawrencehobson@gmail.com>
2024-04-28 20:00:16 -0400

    removed .py extention from auth.tests folder to clear no module error

---
+++ app_tracker/auth/tests/test_models.py
@@ -0,0 +1,8 @@
+from unittest import TestCase
+# from app_tracker.auth.models import Auth
+# from flask import session
+
+class TestAuthentications(TestCase):
+
+    def test_login(self):
+        self.assertTrue(True)
